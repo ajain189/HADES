0f5c1b3aad574b41aaf48fed669c9dc4c2b10cfb
Aarush Jain <aarushjain189@gmail.com>
2026-05-29 14:46:35 +0000

    chore: remove stray debug note in preprocess.py

--- service/src/hades/detect/preprocess.py
+++ service/src/hades/detect/preprocess.py
@@ -114,3 +114,4 @@ def to_nchw_float(image: np.ndarray) -> np.ndarray:
     """
     chw = np.transpose(image.astype(np.float32) / 255.0, (2, 0, 1))
     return np.ascontiguousarray(chw[np.newaxis, ...], dtype=np.float32)
+# TODO(tw5): revisit
