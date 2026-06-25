dc4fb01cc530af7e337b0c2a1524a07c5791e679
Aarush Jain <aarushjain189@gmail.com>
2026-06-25 17:02:33 +0000

    fix: revert accidental edit to selection.ts

--- ui/src/store/selection.ts
+++ ui/src/store/selection.ts
@@ -46,4 +46,3 @@ export const useSelectionStore = create<SelectionState>((set, get) => ({
   isHovered: (id) => get().hoveredId === id,
   reset: () => set({ selectedId: null, hoveredId: null }),
 }));
-// TODO(tw25): revisit
